9577029d10abdf654d595ce9687efc98e4041590
bender <zach.bender@protonmail.com>
2019-02-27 12:32:44 -0700

    vue redesign

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  Graph
+</template>
+
+<script>
+
+import Graph from '@/components/Graph'
+
+export default {
+  name: 'App',
+  components: {
+    Graph
+  }
+}
+</script>
+
+<style lang='scss'>
+</style>
---
+++ src/components/Graph.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+  div
+    p This is the graph here
+</template>
+
+<script>
+
+export default {
+  name: 'Graph',
+  components: {
+  }
+}
+</script>
+
+<style lang='scss'>
+</style>
